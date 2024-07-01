14dc13322005fdf5e5bea4f9c7e6f5a2b71d9fa0
Bibek Thapa <bibekthapa00f@gmail.com>
2024-07-01 15:57:45 +0545

    Some changes related to ui is made

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -57,7 +57,18 @@ const Home = () => {
   return (
     <>
       <Slider images={images} />
-      <div className=" pt-3 text-center">
+      <div className="info-section mt-4">
+        <h1 className="text-center ">Welcome to Purbi Nepal</h1>
+        <p className="container px-1 px-lg-5 text-center">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Perspiciatis, repellat! Magni repudiandae fuga consectetur sunt ex
+          asperiores, aliquam numquam atque. Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Aut dolorum in iste et. Esse repudiandae
+          repellendus distinctio at reprehenderit non, soluta ea sapiente
+          incidunt cumque totam rem praesentium ratione ad.
+        </p>
+      </div>
+      <div className="destination-section pt-3 text-center">
         <h1>Explore the Featured Destinations</h1>
         <div className="card-container container-fluid">
           <div className="row">
@@ -68,35 +79,17 @@ const Home = () => {
                 key={card.name}
               >
                 <img src={card.img} className="card-img-top mt-1" alt="..." />
-                <div className="card-body">
-                  <h5 className="card-title">{card.title}</h5>
+                <div className="card-body py-2 px-2">
+                  <h5 className="card-title mb-0 pb-1">{card.name}</h5>
                   <p className="card-text">{card.description}</p>
                 </div>
-
-                <div className="card-body">
-                  <a href="/" className="card-link">
-                    Card link
-                  </a>
-                  <a href="/" className="card-link">
-                    Another link
-                  </a>
-                </div>
+                <button className="mb-2">Read More</button>
               </div>
             ))}
           </div>
         </div>
       </div>
-      <div className=" info-section mt-5">
-        <h1 className="text-center">Welcome to Purbi Nepal</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Perspiciatis, repellat! Magni repudiandae fuga consectetur sunt ex
-          asperiores, aliquam numquam atque. Lorem ipsum dolor sit, amet
-          consectetur adipisicing elit. Aut dolorum in iste et. Esse repudiandae
-          repellendus distinctio at reprehenderit non, soluta ea sapiente
-          incidunt cumque totam rem praesentium ratione ad.
-        </p>
-      </div>
+      
  
     </>
   );
