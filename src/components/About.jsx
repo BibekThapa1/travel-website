fb757530b2d633ea1a8a74ab401912daacd863b0
Bibek Thapa <bibekthapa00f@gmail.com>
2024-07-05 00:17:38 +0545

    Hello

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <>
-    <div className="about-section container-fluid">
+    <div className="about-section container">
         <h1 className='text-center py-xl-2'>About Us</h1>
         <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed officia reprehenderit nesciunt corporis veniam deleniti vitae quia possimus ullam neque consequuntur, distinctio aliquid impedit vel? Placeat quis laborum nam voluptatum quo fugit illo, aut ipsum optio commodi accusantium id cumque expedita, officiis, unde quod? Consequatur ducimus facilis quam minus ratione.</p>
     </div>
@@ -11,4 +11,5 @@ const About = () => {
   )
 }
 
-export default About
+export default About;
+
